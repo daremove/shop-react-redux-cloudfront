f42cabc72e628806e3e762721047751c1040c247
daremove <daremove1@gmail.com>
2026-05-04 22:54:50 +0500

    Added task 5

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -3,7 +3,9 @@ const API_PATHS = {
     import.meta.env.VITE_PRODUCT_API_PREFIX
   }.execute-api.eu-west-1.amazonaws.com/dev`,
   order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  import: "https://.execute-api.eu-west-1.amazonaws.com/dev",
+  import: `https://${
+    import.meta.env.VITE_IMPORT_API_PREFIX
+  }.execute-api.eu-west-1.amazonaws.com/dev`,
   bff: "https://.execute-api.eu-west-1.amazonaws.com/dev",
   cart: "https://.execute-api.eu-west-1.amazonaws.com/dev",
 };
